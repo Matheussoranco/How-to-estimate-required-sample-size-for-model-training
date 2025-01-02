84efbc49c25895384498b4e81956e852e3d327e1
Matheus Pullig Soranço de Carvalho <matheussoranco@gmail.com>
2025-01-02 15:12:18 -0300

    TF, numPy and ambience setup

---
+++ estimating.py
@@ -0,0 +1,47 @@
+import os
+
+os.environ["KERAS_BACKEND"] = "tensorflow"
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import keras
+from keras import layers
+import tensorflow_datasets as tfds
+
+seed = 42
+keras.utils.set_random_seed(seed)
+AUTO = tf.data.AUTOTUNE
+
+dataset_name = "tf_flowers"
+batch_size = 64
+image_size = (224, 224)
+
+(train_data, test_data), ds_info = tfds.load(
+    dataset_name,
+    split=["train[:90%]", "train[90%:]"],
+    shuffle_files=True,
+    as_supervised=True,
+    with_info=True,
+)
+
+num_classes = ds_info.features["label"].num_classes
+class_names = ds_info.features["label"].names
+
+print(f"Number of classes: {num_classes}")
+print(f"Class names: {class_names}")
+
+
+def dataset_to_array(dataset, image_size, num_classes):
+    images, labels = [], []
+    for img, lab in dataset.as_numpy_iterator():
+        images.append(tf.image.resize(img, image_size).numpy())
+        labels.append(tf.one_hot(lab, num_classes))
+    return np.array(images), np.array(labels)
+
+
+img_train, label_train = dataset_to_array(train_data, image_size, num_classes)
+img_test, label_test = dataset_to_array(test_data, image_size, num_classes)
+
+num_train_samples = len(img_train)
+print(f"Number of training sample: {num_train_samples}")
